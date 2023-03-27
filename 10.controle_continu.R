4dd79df923105e159c9af4c0764ed1207a50da16
Guillaume-Papuga <guillaume.papuga@gmail.com>
2023-03-27 15:52:39 +0200

    test git

--- 10.controle_continu.R
+++ 10.controle_continu.R
@@ -27,7 +27,7 @@ subj = read.csv(here::here("data", "cc1", "tab.sujets.csv"),
 ### Start the loop
 # nb.dataset = length(unique(std.data$equipe))
 
-for (i in 1:3){
+for (i in 1:nrow(subj)){
   ### Generate
   # Submatrix for site 1
   study = subj[i,]
@@ -38,6 +38,7 @@ for (i in 1:3){
                               ncol = 0))
   
   ############################# 1. sites #############################
+  # Creaton des sites randoms
   site = c(as.character (rep (study$zones1, 10)),
            as.character (rep (study$zones2, 10)),
            as.character (rep (study$zones3, 10)),
@@ -45,7 +46,9 @@ for (i in 1:3){
            as.character (rep (study$zones5, 10)),
            as.character (rep (study$zones6, 10)))
   
-  mat = cbind (mat, site)
+  code = paste (site, rep(1:10, 6), sep = "_") # crée un code par plot
+
+  mat = cbind (mat, site, code)
   
   ############################# 2. traitement ##########################
   traitement = matrix(ncol = 1, nrow = 60, data = c(c(rep (1, 10), # generate the ordering sequence
@@ -68,9 +71,9 @@ for (i in 1:3){
   mat = cbind(mat, traitement)
   
   ############################# 3. Variable continue 1 #############################
-  var.c = as.data.frame(rnorm(n = 60, 
-                              mean = study$var.cont_val, 
-                              sd = study$var.cont_val*0.10))
+  var.c = as.data.frame(round(rnorm(n = 60, 
+                                    mean = study$var.cont_val, 
+                                    sd = study$var.cont_val*0.10), 2)) # 10% de variance autour de la moyenne
   names(var.c) = as.character(study$var.cont_name)
   mat = cbind(mat, var.c)
   
@@ -80,8 +83,8 @@ for (i in 1:3){
                     mean = sample(1:60, 1), # chose one value as the peak
                     sd = sample(1:15, 1)) # define a more or less large effect
   temp.trend =  temp.dens * (study$temp_val*0.2 / max(temp.dens)) # max de la tendance à 20% de la moyenne
-  
-  # generate some noise
+
+    # generate some noise
   temp.noise = as.data.frame(rnorm(n = 60,
                                    mean = study$temp_val, # valeur moyenne du tableau
                                    sd = study$temp_val*0.05)) # ecart type de 5% autour de la moyenne
@@ -90,12 +93,11 @@ for (i in 1:3){
   temp = as.data.frame(round(temp.trend + temp.noise[,1], 2))
   names(temp) = "temperature"
   mat = cbind(mat, temp)
-  plot(temp$temperature)
-  
+
   ############################# 5. Les espèces #############################
   ######## sp1 ########
   # sp1 > pic d'abondance sur une serie
-  mean_ab.sp1 = study$abun.moy * (runif(1, min=0.1, max=1.5)) # abondance moyenne de l'espece entre 10 et 150%
+  mean_ab.sp1 = study$abun.moy * (runif(1, min=0.1, max=1)) # abondance moyenne de l'espece entre 10 et 150%
   
   # generate a trend
   sp1.dens = dnorm(seq(1, 60, by = 1), # sequence all data from 1 to 60
@@ -120,13 +122,16 @@ for (i in 1:3){
   sp2 = as.data.frame(round(abs(rnorm(n = 60,
                                       mean = mean_ab.sp2, 
                                       sd = mean_ab.sp2*0.5)), 0))
-  
   # combine the two series
   names(sp2) = "sp2"
   
+  # remplace des valeurs par 0 
+  spot_0 = sample(1:60, 15, replace = FALSE)
+  sp2$sp2[spot_0]=0
+  
   ######## sp3 ########
   # sp3 > negative linear trend
-  mean_ab.sp3 = study$abun.moy * (runif(1, min=0.1, max=3)) # definit abondance moyenne de l'espece
+  mean_ab.sp3 = study$abun.moy * (runif(1, min=0.1, max=1.2)) # definit abondance moyenne de l'espece
   
   # generate a trend
   slp = -0.15 # pente de la tendance
@@ -173,10 +178,13 @@ for (i in 1:3){
   # combine the two series
   names(sp5) = "sp5"
   
+  # remplace des valeurs par 0 
+  spot_0 = sample(1:60, 8, replace = FALSE)
+  sp5$sp5[spot_0]=0
+  
   ######## sp6 ########
   # sp6 > positive linear trend
-  
-  mean_ab.sp6 = study$abun.moy * (runif(1, min=0.1, max=2)) # definit abondance moyenne de l'espece
+  mean_ab.sp6 = study$abun.moy * (runif(1, min=0.1, max=1)) # definit abondance moyenne de l'espece
   
   # generate a trend
   slp = 0.1
@@ -203,6 +211,9 @@ for (i in 1:3){
   # combine the two series
   names(sp7) = "sp7"
   
+  # remplace des valeurs par 0 
+  spot_0 = sample(1:60, 30, replace = FALSE)
+  sp7$sp7[spot_0]=0
   
   ######## sp8 ########
   # sp8 > random rare
@@ -217,21 +228,20 @@ for (i in 1:3){
   # combine the two series
   names(sp8) = "sp8"
   
+  # remplace des valeurs par 0 
+  spot_0 = sample(1:60, 5, replace = FALSE)
+  sp8$sp8[spot_0]=0
+  
   ############################## Synthese #############################
   mat.complet = mat %>%
-    arrange(mat[,2]) %>% # use the second column to sort the dataset before pasting the species
+    arrange(mat[,3]) %>% # use the second column to sort the dataset before pasting the species (and create trends within the datasets)
     mutate(sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8) %>%
     arrange(site)
-  
-  ############################# Save as CSV file ########################
+   ############################# Save as CSV file ########################
   write.csv(x = mat.complet,
-            file = here::here("data", "cc1", "2022", "raw.datasets", paste(row.names(study), ".csv", sep = "")))
+            file = here::here("data", "cc1", "2023", "raw.datasets", 
+                              paste("2023_", row.names(study), ".csv", sep = "")))
   
 } ## loop end here
 
-
-### plots
-boxplot (sp8~ peche, data = mat.complet, 
-         main = "sp8")
-plot(sp8~salinite, data = mat.complet)
 
--- 11.controle_continu_altered.dataset.R
+++ 11.controle_continu_altered.dataset.R
@@ -12,6 +12,8 @@
 # Packages
 library(tidyverse)
 library(here)
+library(stringr)
+library(officer)
 
 
 #####
@@ -19,26 +21,64 @@ library(here)
 #####
 # Loop
 # list of all datasets
-lst.subj = list.files(here::here("data", "cc1", "2022", "raw.datasets"))
+lst.subj = list.files(here::here("data", "cc1", "2023", "raw.datasets"))
 
-# for (i in lst.subj) {}
-
-### Upload the dataset
-mat = read.csv(file = here::here("data", "cc1", "2022", "raw.datasets", i), 
-               header = T, row.names = 1)
-
-############################# 1. Changer 10 decimal au hasard ##########################
-############################# 2. Facteur 10 sur var.cont 1 #############################
-############################# 3. Facteur 100 sur var.cont 2 (tout  un site) ############
-############################# 4. Enlever les 0 #########################################
-############################# 5. Passer en majuscule un site ###########################
-############################# 6. Mettre des majuscules / espaces au oui-non ############
-############################# 7. Sauter des lignes #####################################
-############################# 8. Modifier les entêtes (ajouter les unités) #############
-############################# 9. Séparer en 2 jeux de données ##########################
-
-# Séparer
-# Trier
-
-### Save as Word file
-
+for (i in lst.subj) {
+  ### Upload the dataset
+  mat1 = read.csv(file = here::here("data", "cc1", "2023", "raw.datasets", i), 
+                 header = T, row.names = 1)
+  mat= mat1
+  ############################# 1. Changer 10 decimal au hasard ##########################
+  lst.mistake = sample(1:60, 10, replace = FALSE) # choisit lignes valeurs à modifier
+  
+  mat [,4] = as.character(mat[,4]) # transforme la colonne en caractère
+  
+  mat [lst.mistake,4] = str_replace_all(mat[lst.mistake,4], # subset du tableau
+                                        "[.]" , # cherche les .
+                                        ",") # remplace par des virgules
+  
+  ############################# 3. Facteur 100 sur var.cont 2 (tout  un site) ############
+  selec_site = sample(c("a", "b", "c", "d", "e", "f"), 1) # selectionne un site au hasard
+  mat[which(mat$site==selec_site), "temperature"] = mat[which(mat$site==selec_site), "temperature"]*100 # multiplie la valeur par 100
+  
+  ############################# 4. Enlever les 0 #########################################
+  mat [mat == 0] = "" # remplacer tous les 0 par des cellules vides
+  
+  ############################# 5. Passer des plots en majuscule #########################
+  maj.count = sample(1:60, 10, replace = FALSE) # définit 10 plots aléatoirement
+  
+  mat$site = as.character(mat$site) # transforme de facteur en caractère pour s'astreindre du nombre de modalités
+  mat[maj.count, "site"] = str_to_upper(as.vector(mat[maj.count, "site"])) # passe en majuscule
+  
+  ############################# 6. Mettre des majuscules aux oui/non #####################
+  maj.count = sample(1:60, 5, replace = FALSE) # définit 10 plots aléatoirement
+  
+  mat[,3] = as.character(mat[,3]) # transforme de facteur en caractère pour s'astreindre du nombre de modalités
+  mat[maj.count, 3] = str_to_upper(as.vector(mat[maj.count, 3]))
+  
+  ############################# 6. Mettre des espaces aux oui-non ########################
+  rand.site = sample(c("a", "b", "c", "d", "e", "f"), 1, replace = FALSE) # définit 1 site aléatirement
+  
+  mat[mat$site==rand.site,3] = paste (mat[mat$site==rand.site,3], " ", sep = "") # selectionne la colonne traitement et ajoute un espace
+  
+  ############################# 8. Ajouter les unités de quelques températures ###########
+  add_unit = sample(1:60, 15, replace = FALSE) # définit une seule fois l'aléatoire
+  mat[add_unit, "temperature"] = paste (mat[add_unit, "temperature"], "°", sep = "") # selectionne la colonne temperature et ajoute un symbole degré
+  mat
+  ############################# 9. Séparer en 2 jeux de données ##########################
+  # Séparer
+  mat_var = mat[, c(1:5)] %>%
+    arrange(site) # trie dans l'ordre des sites
+  mat_sp = mat[, c("code", "sp1", "sp2", "sp3", "sp4", "sp5", "sp6", "sp7", "sp8")]%>%
+    arrange(sp6) # trie dans un ordre quelconque = esp5
+  
+  # Enregistrer en Word
+  doc_table <- read_docx (path = here::here("data", "cc1", "template.docx")) %>% # charge le template
+    body_add_table(mat_sp, style = NULL, first_column = TRUE) %>% # ajoute la table espèces
+    body_add_break() %>% # saute une ligne
+    body_add_table(mat_var, style = NULL, first_column = TRUE) # ajoute la table variables
+  
+  print(doc_table, target = here::here("data", "cc1", "2023", "altered.datasets", 
+                                       paste ("data_etud_", unlist(strsplit(i, split = "[.]"))[1], ".docx", sep = "")))
+  
+} ## Loop ends here
